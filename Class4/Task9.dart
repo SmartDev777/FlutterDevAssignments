f367300fab520317563fc8e88c9bb393e5ee9c9a
Smart Dev <developertouseeq@gmail.com>
2022-03-19 14:20:43 +0500

    class 4 assignment  Task 9 vowel checker added

---
+++ Class4/Task9.dart
@@ -0,0 +1,8 @@
+/**
+ * Write a program that takes a character (I. e. string of length 1) and
+returns true if it is a vowel, false otherwise.
+ */
+
+void main(List<String> args) {
+  ['a', 'e', 'i', 'o', 'u'].contains('z') ? print("Vowel") : print("not vowel");
+}
